adbb77f3916d310439e86c3ae52cbdd8bb01b732
Muneeza Syed <161670891+amazingcodemuneeza@users.noreply.github.com>
2026-07-11 15:54:43 +0100

    Delete SQL Files directory

--- SQL Files/01_data_check.sql
+++
@@ -1,8 +0,0 @@
--- ============================================
--- BASIC DATA CHECK
--- Purpose: Understanding dataset structure
--- ============================================
-
-SELECT *
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-LIMIT 10;
