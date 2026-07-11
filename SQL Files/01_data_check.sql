ba39e9fc1822eb800a79aa356ef4ed8c82de2460
Muneeza Syed <161670891+amazingcodemuneeza@users.noreply.github.com>
2026-07-11 14:33:39 +0100

    Create 01_data_check.sql

---
+++ SQL Files/01_data_check.sql
@@ -0,0 +1,8 @@
+-- ============================================
+-- BASIC DATA CHECK
+-- Purpose: Understand dataset structure
+-- ============================================
+
+SELECT *
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+LIMIT 10;
